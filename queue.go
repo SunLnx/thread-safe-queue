f7708f6b142326010e92ce336e45282b939e6db8
黄志武 <huangzhiwu@exatech.cn>
2015-09-12 10:40:16 +0800

    initial

---
+++ queue.go
@@ -0,0 +1,90 @@
+package queue
+
+import (
+	"fmt"
+	"sync"
+)
+
+func NewQueue() *Queue {
+	head := &Node{}
+	return &Queue{
+		head:   head,
+		tail:   head,
+		length: 0,
+		lock:   &sync.Mutex{},
+	}
+}
+
+type Queue struct {
+	head   *Node
+	tail   *Node
+	length int
+
+	lock *sync.Mutex
+}
+
+type Node struct {
+	value interface{}
+	forth *Node
+	back  *Node
+}
+
+func (q *Queue) Enqueue(value interface{}) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+	node := &Node{
+		value: value,
+	}
+	q.tail.back = node
+	node.forth = q.tail
+	q.tail = node
+	q.length++
+}
+
+func (q *Queue) Dequeue() interface{} {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+	if q.head == q.tail {
+		return nil
+	}
+	value := q.head.back.value
+
+	q.head.back = q.head.back.back
+	if q.head.back == nil {
+		q.tail = q.head
+	} else {
+		q.head.back.forth = q.head
+	}
+	q.length--
+	return value
+}
+
+func (q *Queue) Empty() bool {
+	return q.head == q.tail
+}
+
+func (q *Queue) Clear() {
+	var node, tmpNode *Node
+	q.lock.Lock()
+	defer q.lock.Unlock()
+	for node = q.head.back; node != nil; {
+		tmpNode = node.back
+		node.forth = nil
+		node.back = nil
+		node = nil
+		node = tmpNode
+	}
+	q.head.back = nil
+	q.tail = q.head
+	q.length = 0
+}
+
+func (q *Queue) Len() int {
+	return q.length
+}
+
+func (q *Queue) SnapShot() {
+	for node := q.head.back; node != nil; node = node.back {
+		fmt.Print(node.value, `    `)
+	}
+}
---
+++ queue_test.go
@@ -0,0 +1,36 @@
+package queue
+
+import (
+	`math/rand`
+	"testing"
+)
+
+func TestQueue(t *testing.T) {
+	var goroutine int = 10
+	var q *Queue = NewQueue()
+	var ch chan byte = make(chan byte, goroutine)
+	for i := 0; i < goroutine; i++ {
+		go func(value interface{}, ch chan byte) {
+			q.Enqueue(rand.Intn(100))
+			ch <- '0'
+
+		}(i, ch)
+	}
+
+	var sum int = 0
+	for {
+		select {
+		case <-ch:
+			sum++
+			if sum == goroutine {
+				q.SnapShot()
+				for value := q.Dequeue(); value != nil; value = q.Dequeue() {
+					//fmt.Println(value)
+				}
+				q.SnapShot()
+				return
+			}
+		}
+	}
+
+}
